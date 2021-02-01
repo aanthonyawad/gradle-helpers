360fe6b153bb6e078f254351b9a57985696aa10a
aanthonyawad <dev.anthony.awad@gmail.com>
2021-02-01 15:00:01 +0200

    remove test

--- src/test/java/com/awad/anthony/demos/DemosApplicationTests.java
+++ src/test/java/com/awad/anthony/demos/DemosApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
