9de1c236f47de6ac10135b874745c68039935418
aanthonyawad <dev.anthony.awad@gmail.com>
2021-02-01 16:05:30 +0200

    add hello controller

---
+++ src/main/java/com/awad/anthony/demos/rest/HelloController.java
@@ -0,0 +1,12 @@
+package com.awad.anthony.demos.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@GetMapping(path = "/")
+	public String hello() {
+		return "hello NSY_205";
+	}
+}
