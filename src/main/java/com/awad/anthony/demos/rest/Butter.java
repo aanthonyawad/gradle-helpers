8e89dfb5b10448b247d3efccbe5901da540f0e4e
aanthonyawad <dev.anthony.awad@gmail.com>
2021-02-04 21:45:38 +0200

    test

--- src/main/java/com/awad/anthony/demos/rest/Butter.java
+++ src/main/java/com/awad/anthony/demos/rest/Butter.java
@@ -2,18 +2,14 @@ package com.awad.anthony.demos.rest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping(value = "/butter")
 public class Butter {
 
-    @GetMapping(value= "/")
+    @GetMapping(value = "/butter")
     public ModelAndView getTestData() {
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("welcome");
-
+        ModelAndView mv = new ModelAndView("butter");
         return mv;
     }
 }
