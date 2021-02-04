47f0cf5f93c1b83d99cd1a914f025cb639f01df7
aanthonyawad <dev.anthony.awad@gmail.com>
2021-02-04 11:15:06 +0200

    test

--- src/main/java/com/awad/anthony/demos/rest/HelloController.java
+++ src/main/java/com/awad/anthony/demos/rest/HelloController.java
@@ -1,6 +1,7 @@
 package com.awad.anthony.demos.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,11 @@ public class HelloController {
 	@GetMapping(path = "/")
 	public String hello() {
 		return "hello NSY_205";
+	}
+	
+
+	@GetMapping(path = "/{name}")
+	public String name(@PathVariable(value = "name") String name) {
+		return "hello name";
 	}
 }
