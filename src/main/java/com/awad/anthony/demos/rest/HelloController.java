f3f007b5ce27f4d035459bf3c46b2b4d2006c118
aanthonyawad <dev.anthony.awad@gmail.com>
2021-02-11 12:36:22 +0200

    added hello name func

--- src/main/java/com/awad/anthony/demos/rest/HelloController.java
+++ src/main/java/com/awad/anthony/demos/rest/HelloController.java
@@ -1,6 +1,7 @@
 package com.awad.anthony.demos.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,10 +11,9 @@ public class HelloController {
 		return "hello NSY_205";
 	}
 	
-//
-//	@GetMapping(path = "/{name}")
-//	public String name(@PathVariable(value = "name") String name) {
-//		return "hello "+ name;
-//	}
-//	
+
+	@GetMapping(path = "hello/{name}")
+	public String name(@PathVariable(value = "name") String name) {
+		return "hello "+ name;
+	}
 }
